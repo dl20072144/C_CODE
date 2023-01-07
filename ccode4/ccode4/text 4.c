49800704d1b1569e4e3a7455a9fa5ef8c9fe594a
dl200723144 <dl20072144@163.com>
2023-01-07 15:00:21 +0800

    2023.1.7

---
+++ ccode4/ccode4/text 4.c
@@ -0,0 +1,80 @@
+//字符串和转义字符
+//字符串：一串字符--用双引号括起来的一串字符
+//转义字符：转变了原来的意思
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+	//字符数组--数组是一组相同类型的元素
+	//字符串在结尾时隐藏了一个"\0"字符，是字符串结束标志
+
+	//char arr[] ="idffasgh";
+	/*char arr1[] = "abc";
+	char arr2[] = { 'a','b','c' };*/
+	//printf("%s\n",arr1 );
+	//printf("%s\n",arr2 );
+
+	//求字符串长度
+	//strlen("")
+	/*int len = strlen("abc");
+	printf("%d\n", len);*/
+
+	/*printf("%d\n",strlen(arr1));
+	printf("%d\n",strlen(arr2));*/
+
+	/*printf("c:\test.c\n");*/
+	/*printf("ab\nsdf");*/
+
+
+	/*printf("are you ok\?\?");*/
+
+	/*printf("%s\n", "dsfg\"");*/
+
+	/*printf("afds\\nsaf");
+	printf("c:\\test.c\\n");*/
+
+	/*printf("\a");*/
+
+
+
+	//printf("%c\n", '\130');//八进制
+	//printf("%c\n", '\102');
+	//printf("%c\n", '\103');
+	//printf("%c\n", '\104');
+	//printf("%c\n", '\105');
+	//printf("%c\n", '\106');
+	//printf("%c\n", '\107');
+	//printf("%c\n", '\108');
+	//printf("%c\n", '\109');
+	//printf("%c\n", '\110');
+	//printf("%c\n", '\111');
+	//printf("%c\n", '\112');
+	//printf("%c\n", '\113');
+	//printf("%c\n", '\114');
+	//printf("%c\n", '\115');
+	//printf("%c\n", '\116');
+	//printf("%c\n", '\117');
+	//printf("%c\n", '\11');
+	//printf("%c\n", '\11');
+	//printf("%c\n", '\120');
+	//printf("%c\n", '\121');
+	//printf("%c\n", '\122');
+	//printf("%c\n", '\123');
+	//printf("%c\n", '\124');
+	//printf("%c\n", '\125');
+	//printf("%c\n", '\126');
+	//printf("%c\n", '\127');
+	//printf("%c\n", '\12');
+	//printf("%c\n", '\12');
+
+	/*printf("%c\n", '\x51');*/
+
+	//注释
+	//printf("%d\n", strlen("c:\test.c\n"));
+	/*printf("%d\n", strlen("c:\test.c\n"));*/
+	
+
+		return 0;
+}
